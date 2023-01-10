d3b3ab767a1033204fec20642dc5e5f45e0836d4
yerdi <yerdi.isman@gmail.com>
2023-01-10 06:01:11 +0600

    first commit

---
+++ ChatAI-SwiftUI/ChatAI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAI_SwiftUIApp.swift
+//  ChatAI-SwiftUI
+//
+//  Created by Yerdaulet Ismanaliyev on 10.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatAI-SwiftUI/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  ChatAI-SwiftUI
+//
+//  Created by Yerdaulet Ismanaliyev on 10.01.2023.
+//
+import OpenAISwift
+import SwiftUI
+
+final class ViewModel: ObservableObject {
+    init(){}
+    
+    private var client: OpenAISwift?
+    
+    func setup() {
+        client = OpenAISwift(authToken: "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")
+    }
+    
+    func send(text: String,
+              completion:@escaping(String)->Void) {
+        client?.sendCompletion(with: text,
+                               maxTokens: 500,
+                               completionHandler: { result in
+            switch result {
+            case .success(let model):
+                let output = model.choices.first?.text ?? ""
+                completion(output)
+            case .failure:
+                break
+            }
+            
+        })
+        
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var models = [String]()
+    
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(models, id:\.self){ string in
+                Text(string)
+            }
+            
+            Spacer()
+            
+            HStack{
+                TextField("Type here ...", text: $text)
+                Button("Send"){
+                    send()
+                }
+            }
+        
+        }
+        .onAppear{
+            viewModel.setup()
+        }
+        .padding()
+        .background(LinearGradient(colors: [.mint, .orange, .white], startPoint: .topLeading, endPoint: .bottomTrailing))
+    }
+    
+    func send(){
+        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        models.append("Me: \(text)")
+        viewModel.send(text: text){ response in
+            DispatchQueue.main.async {
+                self.models.append("ChatGPT: "+response)
+                self.text = ""
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
